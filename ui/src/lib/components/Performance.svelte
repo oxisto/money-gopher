991817687408003bf36ecaac256ba0ccc03864f1
Christian Banse <oxisto@aybaze.com>
2023-06-17 12:50:47 +0200

    Calculating profit/loss on backend

--- ui/src/lib/components/Performance.svelte
+++ ui/src/lib/components/Performance.svelte
@@ -6,26 +6,21 @@
 
 	export let snapshot: PortfolioSnapshot;
 	export let icon: boolean = true;
-
-	$: perf =
-		((snapshot.totalMarketValue - snapshot.totalPurchaseValue) / snapshot.totalPurchaseValue) * 100;
-	$: perfAbs = snapshot.totalMarketValue - snapshot.totalPurchaseValue;
 </script>
 
 <div
-	class="{perf < 0 ? 'text-red-400' : 'text-green-400'} 
+	class="{snapshot.totalGains < 0 ? 'text-red-400' : 'text-green-400'} 
                flex items-center"
 >
 	{#if icon}
 		<Icon
-			src={perf > 0 ? ArrowTrendingUp : ArrowTrendingDown}
-			class="{perf < 0 ? 'text-red-400' : 'text-green-400'} 
+			src={snapshot.totalGains > 0 ? ArrowTrendingUp : ArrowTrendingDown}
+			class="{snapshot.totalGains < 0 ? 'text-red-400' : 'text-green-400'} 
                  mr-1.5 h-5 w-5 flex-shrink-0"
 			aria-hidden="true"
 		/>
 	{/if}
-	{Intl.NumberFormat(navigator.language, { maximumFractionDigits: 2 }).format(perf)} % ({currency(
-		perfAbs,
-		'EUR'
-	)})
+	{Intl.NumberFormat(navigator.language, { maximumFractionDigits: 2 }).format(
+		snapshot.totalGains * 100
+	)} % ({currency(snapshot.totalProfitOrLoss, 'EUR')})
 </div>
