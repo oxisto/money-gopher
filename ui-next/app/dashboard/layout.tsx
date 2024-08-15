d9ae90ec68eeefce941eed764c4038267c689914
Christian Banse <oxisto@aybaze.com>
2024-08-15 15:24:52 +0200

    Progressing with react

--- ui-next/app/dashboard/layout.tsx
+++ ui-next/app/dashboard/layout.tsx
@@ -1,14 +1,20 @@
+import Nav from "@/app/ui/nav"
+
 export default function DashboardLayout({
-    children, // will be a page or nested layout
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <section>
-        {/* Include shared UI here e.g. a header or sidebar */}
-        <nav>Nav nav here</nav>
-   
-        {children}
-      </section>
-    )
-  }
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section>
+      <Nav />
+
+      <main className="py-10 lg:pl-72">
+        <div className="px-4 sm:px-6 lg:px-8">
+          {children}
+        </div>
+      </main>
+
+    </section>
+  )
+}
